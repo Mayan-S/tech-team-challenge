79daf2d60f18f6d64036f90d7aed22bd3e3483aa
Mayan Saravanabavan <mayansaravanabavan@gmail.com>
2025-05-29 02:15:48 -0400

    commit main

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
 import React from 'react';
 import './App.css';
-import data from './members_details.json'
+import members from './members_details.json';
 
 function App() {
-  return (
-    <div className="App">
+return (
+<div className="App">
+  <header className="header">
+    <h1>👷‍♂️ F!ROSH Orientation Committee 👷‍♀️</h1>
+  </header>
+
+  <div className="card-container">
+    {members.map((member, index) => (
+    <div className="card" key={index}>
+      <img
+        src={'/' + member.picture}
+        alt={member.name}
+        className="card-image"
+      />
+
+      <div className="card-content">
+        <h2 className="card-name">{member.name}</h2>
+        <p className="card-position">{member.position}</p>
+      </div>
     </div>
-  );
+    ))}
+  </div>
+
+  <footer className="footer">
+    <p>© 2025 F!ROSH</p>
+  </footer>
+</div>
+);
 }
 
 export default App;
